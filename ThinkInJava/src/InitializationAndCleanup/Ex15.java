7a607266d221924084fe027f9defa3cee0cbd191
lbuthman <lbuthman@gmail.com>
2017-03-22 21:14:32 -0500

    Add Ex14, 15, and 17, Initialization and Cleanup

---
+++ ThinkInJava/src/InitializationAndCleanup/Ex15.java
@@ -0,0 +1,20 @@
+package InitializationAndCleanup;
+
+/**
+ * Created by lbuthman on 3/22/17.
+ *
+ * demonstrate non-static instance initialization
+ */
+public class Ex15 {
+
+    String s;
+
+    {
+        s = "chocolate!";
+        System.out.println("I love " + s);
+    }
+
+    public static void main(String[] args) {
+        new Ex15();
+    }
+}
